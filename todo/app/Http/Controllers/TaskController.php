2d86540144bac0730b024b3e246f40e65708fe32
Atsushi <>
2020-02-01 13:12:32 +0800

    createのコントローラーとview作成

--- todo/app/Http/Controllers/TaskController.php
+++ todo/app/Http/Controllers/TaskController.php
@@ -14,4 +14,9 @@ class TaskController extends Controller
         return view('tasks.index', compact('tasks'));
 
     }
+
+    public function create()
+    {
+        return view('tasks.create');
+    }
 }
