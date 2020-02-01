b6d97dda18bd2f6a63094dac968f8799337643c4
Atsushi <>
2020-02-01 13:21:49 +0800

    共通部分の切り出し

--- todo/resources/views/tasks/index.blade.php
+++ todo/resources/views/tasks/index.blade.php
@@ -1,26 +1,6 @@
 @extends('layouts.app')
 
 @section('content')
-    <div class="row">
-        <div class="col-12">
-            <nav class="navbar navbar-dark bg-dark">
-            <a href="{{ url('/') }}" class="navbar-brand">Todo</a>
-                <ul class="nav nav-pills">
-                    <li class="nav-item">
-                    <a class="nav-link text-light" href="{{ route('task.create') }}">Create</a>
-                    </li>
-                    <li class="nav-item">
-                        <form class="form-inline">
-                            <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
-                            <button class="btn btn-outline-light my-2 my-sm-0" type="submit">Search</button>
-                        </form>
-                    </li>
-                </ul>
-
-            </nav>
-        </div>
-    </div>
-
     <div class="row p-3">
         @foreach($tasks as $task)
             <div class="col-sm-6 col-md-4 col-lg-3 py-3 py-3">
