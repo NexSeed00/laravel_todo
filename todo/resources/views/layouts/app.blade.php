57cb5496e56e846ca00ab3a98b73f98a0db9e376
Atsushi <>
2020-02-01 13:19:01 +0800

    viewの共通部分を取り出し

---
+++ todo/resources/views/layouts/app.blade.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    <title>{{ config('app.name', 'Laravel') }}</title>
+
+    <!-- Scripts -->
+    <script src="https://ajax.googleapis.com/ajax/libs/ext-core/3.1.0/ext-core.js" defer></script>
+
+    <!-- Fonts -->
+    <link rel="dns-prefetch" href="//fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet" type="text/css">
+
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.2/css/all.css" integrity="sha384-oS3vJWv+0UjzBfQzYUhtDYW+Pj2yciDJxpsK1OYPAYjqT085Qq/1cq5FLXAZQ7Ay" crossorigin="anonymous">
+
+    <!-- Styles -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+</head>
+
+<body>
+    <div class="container-fulid">
+        @yield('content')
+    </div>
+</body>
+
+</html>
--- todo/resources/views/tasks/index.blade.php
+++ todo/resources/views/tasks/index.blade.php
@@ -1,75 +1,46 @@
-<!DOCTYPE html>
-<html lang="en">
+@extends('layouts.app')
 
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+@section('content')
+    <div class="row">
+        <div class="col-12">
+            <nav class="navbar navbar-dark bg-dark">
+            <a href="{{ url('/') }}" class="navbar-brand">Todo</a>
+                <ul class="nav nav-pills">
+                    <li class="nav-item">
+                    <a class="nav-link text-light" href="{{ route('task.create') }}">Create</a>
+                    </li>
+                    <li class="nav-item">
+                        <form class="form-inline">
+                            <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+                            <button class="btn btn-outline-light my-2 my-sm-0" type="submit">Search</button>
+                        </form>
+                    </li>
+                </ul>
 
-    <!-- CSRF Token -->
-    <meta name="csrf-token" content="{{ csrf_token() }}">
-
-    <title>{{ config('app.name', 'Laravel') }}</title>
-
-    <!-- Scripts -->
-    <script src="https://ajax.googleapis.com/ajax/libs/ext-core/3.1.0/ext-core.js" defer></script>
-
-    <!-- Fonts -->
-    <link rel="dns-prefetch" href="//fonts.gstatic.com">
-    <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet" type="text/css">
-
-    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.2/css/all.css" integrity="sha384-oS3vJWv+0UjzBfQzYUhtDYW+Pj2yciDJxpsK1OYPAYjqT085Qq/1cq5FLXAZQ7Ay" crossorigin="anonymous">
-
-    <!-- Styles -->
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
-</head>
-
-<body>
-    <div class="container-fulid">
-        <div class="row">
-            <div class="col-12">
-                <nav class="navbar navbar-dark bg-dark">
-                <a href="{{ url('/') }}" class="navbar-brand">Todo</a>
-                    <ul class="nav nav-pills">
-                        <li class="nav-item">
-                        <a class="nav-link text-light" href="{{ route('task.create') }}">Create</a>
-                        </li>
-                        <li class="nav-item">
-                            <form class="form-inline">
-                                <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
-                                <button class="btn btn-outline-light my-2 my-sm-0" type="submit">Search</button>
-                            </form>
-                        </li>
-                    </ul>
-
-                </nav>
-            </div>
-        </div>
-
-        <div class="row p-3">
-            @foreach($tasks as $task)
-                <div class="col-sm-6 col-md-4 col-lg-3 py-3 py-3">
-                    <div class="card">
-                        <img src="https://picsum.photos/200" class="card-img-top" alt="...">
-                        <div class="card-body">
-                            <h5 class="card-title"><?= $task->title; ?></h5>
-                            <p class="card-text">
-                                <?= $task->contents; ?>
-                            </p>
-                            <div class="text-right d-flex justify-content-end">
-                                <a href="" class="btn text-success">EDIT</a>
-                                <form action="" action="post">
-                                    <input type="hidden" name="id" value="<?= $task->id; ?>">
-                                    <button type="submit" class="btn text-danger">DELETE</button>
-                                </form>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            @endforeach
+            </nav>
         </div>
     </div>
 
-
-</body>
-
-</html>
+    <div class="row p-3">
+        @foreach($tasks as $task)
+            <div class="col-sm-6 col-md-4 col-lg-3 py-3 py-3">
+                <div class="card">
+                    <img src="https://picsum.photos/200" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title"><?= $task->title; ?></h5>
+                        <p class="card-text">
+                            <?= $task->contents; ?>
+                        </p>
+                        <div class="text-right d-flex justify-content-end">
+                            <a href="" class="btn text-success">EDIT</a>
+                            <form action="" action="post">
+                                <input type="hidden" name="id" value="<?= $task->id; ?>">
+                                <button type="submit" class="btn text-danger">DELETE</button>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        @endforeach
+    </div>
+@endsection
