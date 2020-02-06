8d2fb7b0c8add73d9e447979592063f99d1275ec
Atsushi <>
2020-02-06 15:42:13 +0800

    ブックマーク用テーブル作成

---
+++ todo/database/migrations/2020_02_06_073606_create_bookmarks_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateBookmarksTable extends Migration
+{
+    /**
+     * マイグレーションファイル作成
+     * php artisan make:migration create_bookmarks_table
+     *
+     * マイグレーションファイル編集
+     *
+     * マイグレーション
+     * php artisan migrate
+     *
+     */
+
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('bookmarks', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->unsignedBigInteger('user_id');
+            $table->unsignedBigInteger('task_id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('bookmarks');
+    }
+}
